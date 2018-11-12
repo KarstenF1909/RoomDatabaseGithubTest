6bc9006ec8056ad8c2a9830461a2099a25d2d4f0
kf <karstenf1909@gmail.com>
2018-11-12 21:49:39 +0100

    Commit 1.1.14 (RecyclerView funktioniert grundlegend)

--- app/src/main/java/com/karstenfischer/room/roomdatabasegithubtest/NoteDatabase.java
+++ app/src/main/java/com/karstenfischer/room/roomdatabasegithubtest/NoteDatabase.java
@@ -46,7 +46,7 @@ public abstract class NoteDatabase extends RoomDatabase {
         @Override
         protected Void doInBackground(Void... voids) {
             noteDao.insert(new Note("Title 1","Description 1",1));
-            noteDao.insert(new Note("Title 2","Description 2",2));
+            noteDao.insert(new Note("Title 2","Du bist so geil!!!",2));
             noteDao.insert(new Note("Title 3","Description 3",3));
             return null;
         }
