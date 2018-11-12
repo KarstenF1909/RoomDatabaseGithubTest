c9371b128f26150a6194b11a4033e55045fb2709
kf <karstenf1909@gmail.com>
2018-11-12 21:22:43 +0100

    Commit 1.1.10 (onCreateViewHolder in NoteAdapter)

---
+++ app/src/main/java/com/karstenfischer/room/roomdatabasegithubtest/NoteAdapter.java
@@ -0,0 +1,49 @@
+package com.karstenfischer.room.roomdatabasegithubtest;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteHolder> {
+
+    private List<Note> notes=new ArrayList<>();
+
+    @NonNull
+    @Override
+    public NoteHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View itemView=LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.note_item,viewGroup,false);
+        return new NoteHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteHolder noteHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class NoteHolder extends RecyclerView.ViewHolder{
+        private TextView tvTitle;
+        private TextView tvDescription;
+        private TextView tvPriority;
+
+        public NoteHolder(@NonNull View itemView) {
+            super(itemView);
+            tvTitle=itemView.findViewById(R.id.tvTitle);
+            tvDescription=itemView.findViewById(R.id.tvDescription);
+            tvPriority=itemView.findViewById(R.id.tvPriority);
+
+        }
+    }
+
+}
